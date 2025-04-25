b3ea9cf347c35723b6f22c2fd32064e371a355af
kazuto <chiloru0414@gmail.com>
2025-04-26 00:48:43 +0900

    かんたが作ってくれたやつに置き換え

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,94 @@
-import Image from "next/image";
+//import Image from "next/image";
+import './style.css'
+import './vars.css'
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+        <div className="background"></div>
+    <div className="frame-7">
+      <div className="frame-6">
+        <div className="line">
+          <span>
+            <span className="line-span">
+              予約はこちらから！
+              <br />
+            </span>
+            <span className="line-span2">（公式LINEにとぶのかな）</span>
+          </span>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <img className="frame" src="frame0.svg" />
+      </div>
+      <div className="ellipse-1"></div>
+      <div className="_2">
+        <span>
+          <span className="_2-span">2</span>
+          <span className="_2-span2">
+            <br />
+          </span>
+          <span className="_2-span3">分待ち</span>
+        </span>
+      </div>
     </div>
+    <header>
+      <div className="header">
+        <img className="humburger" src="humburger0.svg" />
+        <div className="div">顔シューティング（仮）</div>
+      </div>
+    </header>
+    <div className="big-logos">
+      <div className="frame-2">
+        <div className="rectangle-1"></div>
+        <div className="div2">でかめのロゴ</div>
+      </div>
+      <div className="_2-i">2I　顔シューティング！！</div>
+    </div>
+    <div className="div3">
+      説明文をここに入力する
+      <br />
+      ああああああああ
+      <br />
+      ダミーテキスト
+      <br />
+      <br />
+      ダミーテキスト１
+      <br />
+      なにを書くべきかわからん
+      <br />
+      <br />
+      ダミーテキスト２
+      <br />
+      そろそろネタがない
+      <br />
+      <br />
+      あのイーハトーヴォの透き通った風
+      <br />
+      夏でも風にうんたらかんたら
+    </div>
+    <div className="buttons">
+      <div className="kuwasiku">
+        <div className="div4">もっと詳しく</div>
+        <img className="frame2" src="frame1.svg" />
+      </div>
+      <div className="for-players">
+        <div className="div4">プレイする人へ</div>
+        <img className="frame3" src="frame2.svg" />
+      </div>
+    </div>
+    <div className="information">
+      <div className="frame-8">
+        <div className="rectangle-2"></div>
+        <div className="div5">スクロールボックス</div>
+      </div>
+      <div className="div6">インフォメーション</div>
+    </div>
+    <div className="maps">
+      <div className="frame-82">
+        <div className="rectangle-2"></div>
+        <div className="div5">スクロールボックス</div>
+      </div>
+      <div className="div7">インフォメーション</div>
+    </div>
+    </>
   );
 }
